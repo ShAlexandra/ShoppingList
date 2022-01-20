444209b0275d6bfcb7d9b6558910cf4c13d7bca0
ShAlexandra <shalexandra2000@gmail.com>
2022-01-20 17:58:05 +0300

    Creating shopList(LiveData)

--- app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
@@ -14,12 +14,23 @@ class MainViewModel : ViewModel() {
 
     private val getShopListUseCase = GetShopListUseCase(repository)
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
-    private val editShopListItemCase = EditShopItemUseCase(repository)
+    private val editShopListItemUseCase = EditShopItemUseCase(repository)
 
     val shopList = MutableLiveData<List<ShopItem>>()
 
     fun getShopList() {
         val list = getShopListUseCase.getShopList()
         shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopListItemUseCase.editShopItem(newItem)
+        getShopList()
     }
 }
